4393c826af9847d41e257aefd731e822799bd806
Thedark1337 <thedark1337@thedark1337.com>
2016-07-24 03:56:51 -0500

    Fixed Cohost icon to only set for cohost, move clear to a function.

--- src/release/plugCubed/CustomChatColors.js
+++ src/release/plugCubed/CustomChatColors.js
@@ -1,10 +1,14 @@
 define(['jquery', 'plugCubed/Class', 'plugCubed/RoomSettings', 'plugCubed/StyleManager', 'plugCubed/Settings', 'plugCubed/Utils'], function($, Class, RoomSettings, Styles, Settings, p3Utils) {
-    var handler = Class.extend({
+    var Handler = Class.extend({
+        clear: function() {
+            Styles.unset(['CCC-text-admin', 'CCC-text-ambassador', 'CCC-text-host', 'CCC-text-cohost', 'CCC-text-manager', 'CCC-text-bouncer', 'CCC-text-residentdj', 'CCC-text-regular', 'CCC-text-you', 'CCC-image-admin', 'CCC-image-ambassador', 'CCC-image-host', 'CCC-image-cohost', 'CCC-image-manager', 'CCC-image-bouncer', 'CCC-image-residentdj']);
+        },
         update: function() {
-            var useRoomSettings = Settings.useRoomSettings[document.location.pathname.split('/')[1]];
+            var useRoomSettings = Settings.useRoomSettings[p3Utils.getRoomID()];
+
             useRoomSettings = !!(useRoomSettings == null || useRoomSettings === true);
 
-            Styles.unset(['CCC-text-admin', 'CCC-text-ambassador', 'CCC-text-host', 'CCC-text-cohost', 'CCC-text-manager', 'CCC-text-bouncer', 'CCC-text-residentdj', 'CCC-text-regular', 'CCC-text-you', 'CCC-image-admin', 'CCC-image-ambassador', 'CCC-image-host', 'CCC-image-cohost', 'CCC-image-manager', 'CCC-image-bouncer', 'CCC-image-residentdj']);
+            this.clear();
 
             if ((useRoomSettings && RoomSettings.chatColors.admin) || Settings.colors.admin !== Settings.colorInfo.ranks.admin.color) {
                 Styles.set('CCC-text-admin', ['#user-panel:not(.is-none) .user > .icon-chat-admin + .name', '#user-lists .user > .icon-chat-admin + .name', '.from-admin .un { color:' + p3Utils.toRGB(Settings.colors.admin !== Settings.colorInfo.ranks.admin.color ? Settings.colors.admin : RoomSettings.chatColors.admin) + '!important; }'].join(',\n'));
@@ -16,7 +20,7 @@ define(['jquery', 'plugCubed/Class', 'plugCubed/RoomSettings', 'plugCubed/StyleM
                 Styles.set('CCC-text-host', ['#user-panel:not(.is-none) .user > .icon-chat-host + .name', '#user-lists .user > .icon-chat-host + .name', '.from-host .un { color:' + p3Utils.toRGB(Settings.colors.host !== Settings.colorInfo.ranks.host.color ? Settings.colors.host : RoomSettings.chatColors.host) + '!important; }'].join(',\n'));
             }
             if ((useRoomSettings && RoomSettings.chatColors.cohost) || Settings.colors.cohost !== Settings.colorInfo.ranks.cohost.color) {
-                Styles.set('CCC-text-cohost', ['#user-panel:not(.is-none) .user > .icon-chat-cohost + .name', '#user-lists .user > .icon-chat-cohost + .name', '.from-cohost .un', '.cohost .icon-chat-host { color:' + p3Utils.toRGB(Settings.colors.cohost !== Settings.colorInfo.ranks.cohost.color ? Settings.colors.cohost : RoomSettings.chatColors.cohost) + '!important; }'].join(',\n'));
+                Styles.set('CCC-text-cohost', ['#user-panel:not(.is-none) .user > .icon-chat-cohost + .name', '#user-lists .user > .icon-chat-cohost + .name', '.from-cohost .un', '.cohost { color:' + p3Utils.toRGB(Settings.colors.cohost !== Settings.colorInfo.ranks.cohost.color ? Settings.colors.cohost : RoomSettings.chatColors.cohost) + '!important; }'].join(',\n'));
             }
             if ((useRoomSettings && RoomSettings.chatColors.manager) || Settings.colors.manager !== Settings.colorInfo.ranks.manager.color) {
                 Styles.set('CCC-text-manager', ['#user-panel:not(.is-none) .user > .icon-chat-manager + .name', '#user-lists .user > .icon-chat-manager + .name', '.from-manager .un { color:' + p3Utils.toRGB(Settings.colors.manager !== Settings.colorInfo.ranks.manager.color ? Settings.colors.manager : RoomSettings.chatColors.manager) + '!important; }'].join(',\n'));
@@ -34,23 +38,30 @@ define(['jquery', 'plugCubed/Class', 'plugCubed/RoomSettings', 'plugCubed/StyleM
                 Styles.set('CCC-text-you', ['#user-lists .list .user.is-you .name', '.from-you .un { color:' + p3Utils.toRGB(Settings.colors.you) + '!important; }'].join(',\n'));
             }
             if (useRoomSettings) {
-                if (RoomSettings.chatIcons.admin)
+                if (RoomSettings.chatIcons.admin) {
                     Styles.set('CCC-image-admin', ['.icon-chat-admin { background-image: url("' + RoomSettings.chatIcons.admin + '"); background-position: 0 0; }'].join(',\n'));
-                if (RoomSettings.chatIcons.ambassador)
+                }
+                if (RoomSettings.chatIcons.ambassador) {
                     Styles.set('CCC-image-ambassador', ['.icon-chat-ambassador { background-image: url("' + RoomSettings.chatIcons.ambassador + '"); background-position: 0 0; }'].join(',\n'));
-                if (RoomSettings.chatIcons.host)
+                }
+                if (RoomSettings.chatIcons.host) {
                     Styles.set('CCC-image-host', ['.icon-chat-host { background-image: url("' + RoomSettings.chatIcons.host + '"); background-position: 0 0; }'].join(',\n'));
-                if (RoomSettings.chatIcons.cohost)
-                    Styles.set('CCC-image-cohost', ['.icon-chat-cohost, .cohost .icon-chat-host { background-image: url("' + RoomSettings.chatIcons.cohost + '"); background-position: 0 0; }'].join(',\n'));
-                if (RoomSettings.chatIcons.manager)
+                }
+                if (RoomSettings.chatIcons.cohost) {
+                    Styles.set('CCC-image-cohost', ['.icon-chat-cohost { background-image: url("' + RoomSettings.chatIcons.cohost + '"); background-position: 0 0; }'].join(',\n'));
+                }
+                if (RoomSettings.chatIcons.manager) {
                     Styles.set('CCC-image-manager', ['.icon-chat-manager { background-image: url("' + RoomSettings.chatIcons.manager + '"); background-position: 0 0; }'].join(',\n'));
-                if (RoomSettings.chatIcons.bouncer)
+                }
+                if (RoomSettings.chatIcons.bouncer) {
                     Styles.set('CCC-image-bouncer', ['.icon-chat-bouncer { background-image: url("' + RoomSettings.chatIcons.bouncer + '"); background-position: 0 0; }'].join(',\n'));
-                if (RoomSettings.chatIcons.residentdj)
+                }
+                if (RoomSettings.chatIcons.residentdj) {
                     Styles.set('CCC-image-residentdj', ['.icon-chat-dj { background-image: url("' + RoomSettings.chatIcons.residentdj + '"); background-position: 0 0; }'].join(',\n'));
+                }
             }
         }
     });
 
-    return new handler();
+    return new Handler();
 });
