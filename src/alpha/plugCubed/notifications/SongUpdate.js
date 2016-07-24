86fb1e329b3cf0e58c516ff523033b08943d1d2b
Thedark1337 <thedark1337@thedark1337.com>
2016-07-24 03:47:54 -0500

    use different IDs so that it doesn't stack different notifications

--- src/alpha/plugCubed/notifications/SongUpdate.js
+++ src/alpha/plugCubed/notifications/SongUpdate.js
@@ -1,10 +1,12 @@
 define(['plugCubed/handlers/TriggerHandler', 'plugCubed/Settings', 'plugCubed/Utils', 'plugCubed/Lang', 'plugCubed/enums/Notifications'], function(TriggerHandler, Settings, p3Utils, p3Lang, enumNotifications) {
-    var handler = TriggerHandler.extend({
+    var Handler = TriggerHandler.extend({
         trigger: API.ADVANCE,
         handler: function(data) {
-            if ((Settings.notify & enumNotifications.SONG_UPDATE) === enumNotifications.SONG_UPDATE && data.media != null && data.dj != null)
-                p3Utils.chatLog(undefined, p3Lang.i18n('notify.message.updates', p3Utils.cleanTypedString(data.media.title), p3Utils.cleanTypedString(data.media.author), p3Utils.cleanTypedString(data.dj.username)), Settings.colors.updates || Settings.colorInfo.notifications.updates.color, -1);
+            if ((Settings.notify & enumNotifications.SONG_UPDATE) === enumNotifications.SONG_UPDATE && data.media != null && data.dj != null) {
+                p3Utils.chatLog(undefined, p3Lang.i18n('notify.message.updates', p3Utils.cleanTypedString(data.media.title), p3Utils.cleanTypedString(data.media.author), p3Utils.cleanTypedString(data.dj.username)), Settings.colors.updates || Settings.colorInfo.notifications.updates.color, -7);
+            }
         }
     });
-    return new handler();
+
+    return new Handler();
 });
--- src/release/plugCubed/notifications/SongStats.js
+++ src/release/plugCubed/notifications/SongStats.js
@@ -1,10 +1,12 @@
 define(['plugCubed/handlers/TriggerHandler', 'plugCubed/Settings', 'plugCubed/Utils', 'plugCubed/Lang', 'plugCubed/enums/Notifications'], function(TriggerHandler, Settings, p3Utils, p3Lang, enumNotifications) {
-    var handler = TriggerHandler.extend({
+    var Handler = TriggerHandler.extend({
         trigger: API.ADVANCE,
         handler: function(data) {
-            if ((Settings.notify & enumNotifications.SONG_STATS) === enumNotifications.SONG_STATS)
-                p3Utils.chatLog(undefined, p3Lang.i18n('notify.message.stats', data.lastPlay.score.positive, data.lastPlay.score.negative, data.lastPlay.score.grabs), Settings.colors.stats || Settings.colorInfo.notifications.stats.color, -1);
+            if ((Settings.notify & enumNotifications.SONG_STATS) === enumNotifications.SONG_STATS && data.lastPlay != null && data.lastPlay.score != null) {
+                p3Utils.chatLog(undefined, p3Lang.i18n('notify.message.stats', data.lastPlay.score.positive, data.lastPlay.score.negative, data.lastPlay.score.grabs), Settings.colors.stats || Settings.colorInfo.notifications.stats.color, -5);
+            }
         }
     });
-    return new handler();
+
+    return new Handler();
 });
