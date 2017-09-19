fc2d4728b3b580fa89a08806b7c07ad3c65028e8
Thedark1337 <thedark1337@thedark1337.com>
2017-09-19 06:59:02 -0500

    Alpha: Fixed spacing in about tab

--- src/alpha/plugCubed/dialogs/panels/About.js
+++ src/alpha/plugCubed/dialogs/panels/About.js
@@ -26,9 +26,9 @@ define(['plugCubed/Class', 'plugCubed/Utils', 'plugCubed/dialogs/ControlPanel', 
 
             $table.append($('<tr>').append($('<td>').attr('colspan', 2).append($('<strong>').text(p3Lang.i18n('panels.about.translations') + ': ')).append($('<span>').css('color', '#FFFFFF').html('<a href="https://crowdin.com/project/plugcubed-website" target="_blank" style="color:#FFFFFF;">' + p3Lang.i18n('panels.about.websitetranslationsText') + '</a>'))));
 
-            $table.append($('<tr>').append($('<td>').attr('colspan', 2).append($('<strong>').text('Discord:')).append($('<span>').css('color', '#FFFFFF').html('<a href="https://plugcubed.net/discord" target="_blank" style="color:#FFFFFF;">plug³ Discord</a>'))));
+            $table.append($('<tr>').append($('<td>').attr('colspan', 2).append($('<strong>').text('Discord: ')).append($('<span>').css('color', '#FFFFFF').html('<a href="https://plugcubed.net/discord" target="_blank" style="color:#FFFFFF;">plug³ Discord</a>'))));
 
-            $table.append($('<tr>').append($('<td>').attr('colspan', 2).append($('<strong>').text('Twitter:')).append($('<span>').css('color', '#FFFFFF').html('<a href="https://twitter.com/plugCubed" target="_blank" style="color:#FFFFFF;">plug³ Twitter</a>'))));
+            $table.append($('<tr>').append($('<td>').attr('colspan', 2).append($('<strong>').text('Twitter: ')).append($('<span>').css('color', '#FFFFFF').html('<a href="https://twitter.com/plugCubed" target="_blank" style="color:#FFFFFF;">plug³ Twitter</a>'))));
 
             $contentDiv.append($('<br>')).append($table);
 
