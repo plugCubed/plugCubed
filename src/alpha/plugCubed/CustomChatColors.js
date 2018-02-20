b2c56238020c5b14a23027bd47dbc1079e3a360c
Thedark1337 <thedark1337@thedark1337.com>
2018-02-19 19:24:01 -0600

    Alpha: Remove dupe plugcubed/Lang require

--- src/alpha/plugCubed/CustomChatColors.js
+++ src/alpha/plugCubed/CustomChatColors.js
@@ -1,67 +1,115 @@
-define(['jquery', 'plugCubed/Class', 'plugCubed/RoomSettings', 'plugCubed/StyleManager', 'plugCubed/Settings', 'plugCubed/Utils'], function($, Class, RoomSettings, Styles, Settings, p3Utils) {
-    var Handler = Class.extend({
-        clear: function() {
-            Styles.unset(['CCC-text-admin', 'CCC-text-ambassador', 'CCC-text-host', 'CCC-text-cohost', 'CCC-text-manager', 'CCC-text-bouncer', 'CCC-text-residentdj', 'CCC-text-regular', 'CCC-text-you', 'CCC-image-admin', 'CCC-image-ambassador', 'CCC-image-host', 'CCC-image-cohost', 'CCC-image-manager', 'CCC-image-bouncer', 'CCC-image-residentdj']);
+define(['jquery', 'plugCubed/Class', 'plugCubed/Lang', 'plugCubed/CustomChatColors', 'plugCubed/Settings', 'plugCubed/Utils'], function($, Class, p3Lang, CCC, Settings, p3Utils) {
+    var Context = window.plugCubedModules.context;
+
+    function guiInput(id, text, defaultColor) {
+        if (!Settings.colors[id]) {
+            Settings.colors[id] = defaultColor;
+        }
+
+        return $('<div class="item">').addClass('p3-s-cc-' + id).append($('<span>').text(text)).append($('<span>').addClass('default').css('display', (p3Utils.equalsIgnoreCase(Settings.colors[id], defaultColor) ? 'none' : 'block')).mouseover(function() {
+            Context.trigger('tooltip:show', p3Lang.i18n('tooltip.reset'), $(this), false);
+        }).mouseout(function() {
+            Context.trigger('tooltip:hide');
+        }).click(function() {
+            $(this).parent().find('input').val(defaultColor);
+            $(this).parent().find('.example').css('background-color', p3Utils.toRGB(defaultColor));
+            $(this).css('display', 'none');
+            Settings.colors[id] = defaultColor;
+            Settings.save();
+            CCC.update();
+        })).append($('<span>').addClass('example').css('background-color', p3Utils.toRGB(Settings.colors[id]))).append($('<input>').val(Settings.colors[id]).keyup(function() {
+            if (p3Utils.isRGB($(this).val())) {
+                $(this).parent().find('.example').css('background-color', p3Utils.toRGB($(this).val()));
+                Settings.colors[id] = $(this).val();
+                Settings.save();
+                CCC.update();
+            }
+            $(this).parent().find('.default').css('display', (p3Utils.equalsIgnoreCase($(this).val(), defaultColor) ? 'none' : 'block'));
+        }));
+    }
+
+    var div;
+
+    var A = Class.extend({
+        render: function() {
+            var i;
+            var $settings = $('#p3-settings');
+
+            if (div != null) {
+                if (div.css('left') === '-500px') {
+                    div.animate({
+                        left: $settings.width() + 1
+                    });
+
+                    return;
+                }
+                div.animate({
+                    left: -div.width() - 2
+                }, {
+                    complete: function() {
+                        if (div) {
+                            div.detach();
+                            div = undefined;
+                        }
+                    }
+                });
+
+                return;
+            }
+            var container = $('<div class="container">').append($('<div class="section">').text(p3Lang.i18n('customchatcolors.userRanksHeader')));
+
+            for (i in Settings.colorInfo.ranks) {
+                if (Settings.colorInfo.ranks.hasOwnProperty(i)) {
+                    container.append(guiInput(i, p3Lang.i18n(Settings.colorInfo.ranks[i].title), Settings.colorInfo.ranks[i].color));
+                }
+            }
+            container.append($('<div class="spacer">').append($('<div class="divider">'))).append($('<div class="section">').text(p3Lang.i18n('customchatcolors.chatnotifs')));
+            for (i in Settings.colorInfo.notifications) {
+                if (Settings.colorInfo.notifications.hasOwnProperty(i)) {
+                    if (i === 'songLength') {
+                        if (API.hasPermission(undefined, API.ROLE.BOUNCER) || p3Utils.isPlugCubedDeveloper()) {
+                            container.append(guiInput(i, p3Lang.i18n(Settings.colorInfo.notifications[i].title, Settings.notifySongLength), Settings.colorInfo.notifications[i].color));
+                        }
+                    } else if (i === 'boothAlert') {
+                        container.append(guiInput(i, p3Lang.i18n(Settings.colorInfo.notifications[i].title, Settings.boothAlert), Settings.colorInfo.notifications[i].color));
+                    } else {
+                        container.append(guiInput(i, p3Lang.i18n(Settings.colorInfo.notifications[i].title), Settings.colorInfo.notifications[i].color));
+                    }
+                }
+            }
+            div = $('<div id="p3-settings-custom-colors" style="left: -500px;">').append($('<div class="header">').append($('<div class="back">').append($('<i class="icon icon-arrow-left"></i>')).click(function() {
+                if (div != null) {
+                    div.animate({
+                        left: -div.width() - 2
+                    }, {
+                        complete: function() {
+                            if (div) {
+                                div.detach();
+                                div = undefined;
+                            }
+                        }
+                    });
+                }
+            })).append($('<div class="title">').append($('<span>').text(p3Lang.i18n('menu.customchatcolors'))))).append(container).animate({
+                left: $settings.width() + 1
+            });
+            $('#p3-settings-wrapper').append(div);
         },
-        update: function() {
-            var useRoomSettings = Settings.useRoomSettings[p3Utils.getRoomID()];
-
-            useRoomSettings = !!(useRoomSettings == null || useRoomSettings === true);
-
-            this.clear();
-
-            if ((useRoomSettings && RoomSettings.chatColors.admin) || Settings.colors.admin !== Settings.colorInfo.ranks.admin.color) {
-                Styles.set('CCC-text-admin', ['#user-rollover .rank-admin .username', '#user-lists .user > .icon-chat-admin + .name', '#footer-user .info .name .icon-chat-admin + span', '#waitlist .icon-chat-admin + span', '.from-admin .un', '.admin { color:' + p3Utils.toRGB(Settings.colors.admin !== Settings.colorInfo.ranks.admin.color ? Settings.colors.admin : RoomSettings.chatColors.admin) + '!important; }'].join(',\n'));
-            }
-            if ((useRoomSettings && RoomSettings.chatColors.ambassador) || Settings.colors.ambassador !== Settings.colorInfo.ranks.ambassador.color) {
-                Styles.set('CCC-text-ambassador', ['#user-rollover .rank-ambassador .username', '#user-lists .user > .icon-chat-ambassador + .name', '#footer-user .info .name .icon-chat-ambassador + span', '#waitlist .icon-chat-ambassador + span', '.from-ambassador .un', '.ambassador { color:' + p3Utils.toRGB(Settings.colors.ambassador !== Settings.colorInfo.ranks.ambassador.color ? Settings.colors.ambassador : RoomSettings.chatColors.ambassador) + '!important; }'].join(',\n'));
-            }
-            if ((useRoomSettings && RoomSettings.chatColors.host) || Settings.colors.host !== Settings.colorInfo.ranks.host.color) {
-                Styles.set('CCC-text-host', ['#user-rollover .rank-host .username', '#user-lists .user > .icon-chat-host + .name', '#footer-user .info .name .icon-chat-host + span', '#waitlist .icon-chat-host + span', '.from-host .un', '.host { color:' + p3Utils.toRGB(Settings.colors.host !== Settings.colorInfo.ranks.host.color ? Settings.colors.host : RoomSettings.chatColors.host) + '!important; }'].join(',\n'));
-            }
-            if ((useRoomSettings && RoomSettings.chatColors.cohost) || Settings.colors.cohost !== Settings.colorInfo.ranks.cohost.color) {
-                Styles.set('CCC-text-cohost', ['#user-rollover .rank-cohost .username', '#user-lists .user > .icon-chat-cohost + .name', '#footer-user .info .name .icon-chat-cohost + span', '#waitlist .icon-chat-cohost + span', '.from-cohost .un', '.cohost { color:' + p3Utils.toRGB(Settings.colors.cohost !== Settings.colorInfo.ranks.cohost.color ? Settings.colors.cohost : RoomSettings.chatColors.cohost) + '!important; }'].join(',\n'));
-            }
-            if ((useRoomSettings && RoomSettings.chatColors.manager) || Settings.colors.manager !== Settings.colorInfo.ranks.manager.color) {
-                Styles.set('CCC-text-manager', ['#user-rollover .rank-manager .username', '#user-lists .user > .icon-chat-manager + .name', '#footer-user .info .name .icon-chat-manager + span', '#waitlist .icon-chat-manager + span', '.from-manager .un', '.manager:not(.list) { color:' + p3Utils.toRGB(Settings.colors.manager !== Settings.colorInfo.ranks.manager.color ? Settings.colors.manager : RoomSettings.chatColors.manager) + '!important; }'].join(',\n'));
-            }
-            if ((useRoomSettings && RoomSettings.chatColors.bouncer) || Settings.colors.bouncer !== Settings.colorInfo.ranks.bouncer.color) {
-                Styles.set('CCC-text-bouncer', ['#user-rollover .rank-bouncer .username', '#user-lists .user > .icon-chat-bouncer + .name', '#footer-user .info .name .icon-chat-bouncer + span', '#waitlist .icon-chat-bouncer + span', '.from-bouncer .un', '.bouncer:not(.list) { color:' + p3Utils.toRGB(Settings.colors.bouncer !== Settings.colorInfo.ranks.bouncer.color ? Settings.colors.bouncer : RoomSettings.chatColors.bouncer) + '!important; }'].join(',\n'));
-            }
-            if ((useRoomSettings && RoomSettings.chatColors.residentdj) || Settings.colors.residentdj !== Settings.colorInfo.ranks.residentdj.color) {
-                Styles.set('CCC-text-residentdj', ['#user-rollover .rank-residentdj .username', '#user-lists .user > .icon-chat-dj + .name', '#footer-user .info .name .icon-chat-dj + span', '#waitlist .icon-chat-dj + span', '.from-dj .un', '.dj { color:' + p3Utils.toRGB(Settings.colors.residentdj !== Settings.colorInfo.ranks.residentdj.color ? Settings.colors.residentdj : RoomSettings.chatColors.residentdj) + '!important; }'].join(',\n'));
-            }
-            if ((useRoomSettings && RoomSettings.chatColors.regular) || Settings.colors.regular !== Settings.colorInfo.ranks.regular.color) {
-                Styles.set('CCC-text-regular', ['#user-rollover .rank-regular .username', '#user-lists .user > .name:first-child', '.from-regular .un', '#waitlist .user .name i:not(.icon) +  span', '.regular { color:' + p3Utils.toRGB(Settings.colors.regular !== Settings.colorInfo.ranks.regular.color ? Settings.colors.regular : RoomSettings.chatColors.regular) + '!important; }'].join(',\n'));
-            }
-            if (Settings.colors.you !== Settings.colorInfo.ranks.you.color) {
-                Styles.set('CCC-text-you', ['#user-rollover.is-you .username', '#user-lists .list .user.is-you .name', '.from-you .un', '.you { color:' + p3Utils.toRGB(Settings.colors.you) + '!important; }'].join(',\n'));
-            }
-            if (useRoomSettings) {
-                if (RoomSettings.chatIcons.admin) {
-                    Styles.set('CCC-image-admin', ['.icon-chat-admin { background-image: url("' + RoomSettings.chatIcons.admin + '"); background-position: 0 0; }'].join(',\n'));
-                }
-                if (RoomSettings.chatIcons.ambassador) {
-                    Styles.set('CCC-image-ambassador', ['.icon-chat-ambassador { background-image: url("' + RoomSettings.chatIcons.ambassador + '"); background-position: 0 0; }'].join(',\n'));
-                }
-                if (RoomSettings.chatIcons.host) {
-                    Styles.set('CCC-image-host', ['.icon-chat-host { background-image: url("' + RoomSettings.chatIcons.host + '"); background-position: 0 0; }'].join(',\n'));
-                }
-                if (RoomSettings.chatIcons.cohost) {
-                    Styles.set('CCC-image-cohost', ['.icon-chat-cohost { background-image: url("' + RoomSettings.chatIcons.cohost + '"); background-position: 0 0; }'].join(',\n'));
-                }
-                if (RoomSettings.chatIcons.manager) {
-                    Styles.set('CCC-image-manager', ['.icon-chat-manager { background-image: url("' + RoomSettings.chatIcons.manager + '"); background-position: 0 0; }'].join(',\n'));
-                }
-                if (RoomSettings.chatIcons.bouncer) {
-                    Styles.set('CCC-image-bouncer', ['.icon-chat-bouncer { background-image: url("' + RoomSettings.chatIcons.bouncer + '"); background-position: 0 0; }'].join(',\n'));
-                }
-                if (RoomSettings.chatIcons.residentdj) {
-                    Styles.set('CCC-image-residentdj', ['.icon-chat-dj { background-image: url("' + RoomSettings.chatIcons.residentdj + '"); background-position: 0 0; }'].join(',\n'));
-                }
+        hide: function() {
+            if (div != null) {
+                div.animate({
+                    left: -div.width() - 2
+                }, {
+                    complete: function() {
+                        if (div) {
+                            div.detach();
+                            div = undefined;
+                        }
+                    }
+                });
             }
         }
     });
 
-    return new Handler();
+    return new A();
 });
